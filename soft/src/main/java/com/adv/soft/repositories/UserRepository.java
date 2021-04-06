19dd11d6bc9d71665a32fb188214f116bddb9df5
JTReacher <66769946+JTR1993@users.noreply.github.com>
2021-04-06 12:11:29 +0100

    controllers v1

--- soft/src/main/java/com/adv/soft/repositories/UserRepository.java
+++ soft/src/main/java/com/adv/soft/repositories/UserRepository.java
@@ -4,11 +4,9 @@ import com.adv.soft.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 //I should be able to define different queries to present whatever
 //Find all users. Find all students.
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     public User findByEmail(String email);
